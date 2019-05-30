188f53b16f63bac67c9edfabb99b5a8233b31b48
Amit Pundir <amit.pundir@linaro.org>
2019-05-30 16:28:06 +0000

    Add flash-all.sh script and qdl tool
    Add flash-all.sh script, README and qdl
    
    Prebuilt flashing tool (qdl) is built from source hosted at
        https://github.com/andersson/qdl/commits/master (760b3dffb03d)
    
    NOTE: This commit does not include the firmware binaries which
    are required. They will be added later when the licensing is
    sorted out.
    
    Change-Id: I0ec3f0b7cae00c606acd1b3f5b74a5fdf73c1162
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>
    [jstultz: Break out from a different patch, and tweaks to script]
    Signed-off-by: John Stultz <john.stultz@linaro.org>

---
+++ installer/linaro_db845c/flash-all-aosp.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+INSTALLER_DIR="`dirname ${0}`"
+
+# for cases that don't run "lunch linaro_db845c-userdebug"
+if [ -z "${ANDROID_BUILD_TOP}" ]; then
+    ANDROID_BUILD_TOP=${INSTALLER_DIR}/../../../../../
+    ANDROID_PRODUCT_OUT="${ANDROID_BUILD_TOP}/out/target/product/linaro_db845c"
+fi
+
+if [ ! -d "${ANDROID_PRODUCT_OUT}" ]; then
+    echo "error in locating out directory, check if it exist"
+    exit
+fi
+
+echo "android out dir:${ANDROID_PRODUCT_OUT}"
+
+# TODO: Pull one-time recovery/qdl path out of standard install
+# Flash bootloader firmware files
+if [ ! -d "${INSTALLER_DIR}/firmware-binaries/" ]; then
+    echo "No firmware directory? Make sure binaries have been provided"
+    exit
+fi
+pushd "${INSTALLER_DIR}"/firmware-binaries/
+sudo "${INSTALLER_DIR}"/qdl prog_firehose_ddr.elf rawprogram[012345].xml patch[012345].xml
+popd
+
+# Flash AOSP images
+# Timeout (atleast 10sec) to let db845c reboot in fastboot mode
+sleep 15
+
+# Set HDMI monitor output
+fastboot oem select-display-panel foobar
+fastboot reboot bootloader
+
+# Slot _a is already marked as active by bootloader but just in case..
+fastboot set_active a
+fastboot flash system_a "${ANDROID_PRODUCT_OUT}"/super.img
+fastboot flash userdata "${ANDROID_PRODUCT_OUT}"/userdata.img
+# Mostly redundant but we run into following flashing error at times
+# FAILED (remote: 'Failed to load/authenticate boot image: Load Error')
+# so set the active slot again just in case..
+fastboot set_active a
+fastboot flash boot "${ANDROID_PRODUCT_OUT}"/boot.img
