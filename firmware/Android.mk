1c3aa4ebddbc8e80447a048f296cb2d37950b81d
Amit Pundir <amit.pundir@linaro.org>
2019-07-09 12:28:43 +0530

    linaro_db845c: firmware: Add Qcom firmware files
    Copied Qcom firmware files and license from RB3_firmware_20190529180356-v2.zip
    http://releases.linaro.org/96boards/dragonboard845c/qualcomm/firmware/RB3_firmware_20190529180356-v2.zip
    
    Also moved Adreno binaries install location from
    /vendor/firmware/qcom/ to /vendor/firmware to match
    install location in OpenEmbedded images (and
    probably Debian too?).
    
    Change-Id: If111c9e8d834043db66f33c29ca47a24442e4ec6
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>

--- firmware/Android.mk
+++ firmware/Android.mk
@@ -1,28 +1,100 @@
-#
-# Copyright (C) 2015 The Android Open-Source Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
-# WARNING: Everything listed here will be built on ALL platforms,
-# including x86, the emulator, and the SDK.  Modules must be uniquely
-# named (liblights.panda), and must build everywhere, or limit themselves
-# to only building on ARM if they include assembly. Individual makefiles
-# are responsible for having their own logic, for fine-grained control.
-
 LOCAL_PATH := $(call my-dir)
 
-# if some modules are built directly from this directory (not subdirectories),
-# their rules should be written here.
+include device/linaro/dragonboard/utils.mk
 
+# Firmware files copied over from
+# http://releases.linaro.org/96boards/dragonboard845c/qualcomm/firmware/RB3_firmware_20190529180356-v2.zip
+
+# Adreno
+firmware_files :=	\
+    a630_gmu.bin	\
+    a630_sqe.fw		\
+    a630_zap.b00	\
+    a630_zap.b01	\
+    a630_zap.b02	\
+    a630_zap.elf	\
+    a630_zap.mdt
+
+# DSP (adsp+cdsp)
+firmware_files +=	\
+    adsp.b00		\
+    adsp.b01		\
+    adsp.b02		\
+    adsp.b03		\
+    adsp.b04		\
+    adsp.b05		\
+    adsp.b06		\
+    adsp.b07		\
+    adsp.b08		\
+    adsp.b09		\
+    adsp.b10		\
+    adsp.b11		\
+    adsp.b12		\
+    adsp.b13		\
+    adsp.mdt		\
+    cdsp.b00		\
+    cdsp.b01		\
+    cdsp.b02		\
+    cdsp.b03		\
+    cdsp.b04		\
+    cdsp.b05		\
+    cdsp.b06		\
+    cdsp.b08		\
+    cdsp.mdt
+
+# USB
+firmware_files +=	\
+    K2026090.mem
+
+# Wlan
+firmware_files +=	\
+    bdwlan.102		\
+    bdwlan.104		\
+    bdwlan.105		\
+    bdwlan.106		\
+    bdwlan.107		\
+    bdwlan.108		\
+    bdwlan.109		\
+    bdwlan.10b		\
+    bdwlan.10c		\
+    bdwlan.b04		\
+    bdwlan.b07		\
+    bdwlan.b09		\
+    bdwlan.b0a		\
+    bdwlan.b0b		\
+    bdwlan.b0d		\
+    bdwlan.b0e		\
+    bdwlan.b0f		\
+    bdwlan.b14		\
+    bdwlan.b15		\
+    bdwlan.b30		\
+    bdwlan.b31		\
+    bdwlan.b32		\
+    bdwlan.b33		\
+    bdwlan.b34		\
+    bdwlan.b35		\
+    bdwlan.b36		\
+    bdwlan.b37		\
+    bdwlan.b38		\
+    bdwlan.b39		\
+    bdwlan.b3a		\
+    bdwlan.b3c		\
+    bdwlan.b3d		\
+    bdwlan.b3e		\
+    bdwlan.b3f		\
+    bdwlan.b41		\
+    bdwlan.b42		\
+    bdwlan.b45		\
+    bdwlan.b70		\
+    bdwlan.bin		\
+    bdwlan.txt		\
+    wlanmdsp.mbn
+
+# License
+firmware_files +=	\
+    LICENSE.qcom.txt
+
+$(foreach f, $(firmware_files), $(call add-qcom-firmware, $(f), $(TARGET_OUT_VENDOR)/firmware/))
+
+# Venus
 include $(call all-makefiles-under,$(LOCAL_PATH))
