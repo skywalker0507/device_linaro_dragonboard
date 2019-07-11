aa646ea4ab9570b8ce74b18e01189249e998c103
John Stultz <john.stultz@linaro.org>
2019-07-11 19:22:10 +0000

    linaro_db845c: flash-all-aosp.sh: Split recovery mode out from standard flash-all-aosp.sh
    To simplify the flashing process, split out the recovery
    steps into a separate helper script and use standard fastboot
    flashing for normal bootloader updates.
    
    Also update documentation to clarify the processes.
    
    Change-Id: If28588d35ee5511bcd700566a79da77910037b95
    Signed-off-by: John Stultz <john.stultz@linaro.org>

--- installer/linaro_db845c/recovery.sh
+++ installer/linaro_db845c/recovery.sh
@@ -2,6 +2,7 @@
 
 INSTALLER_DIR="`dirname ${0}`"
 
+QDL="`readlink -f ${INSTALLER_DIR}/qdl`"
 FIRMWARE_DIR="dragonboard-845c-bootloader-ufs-aosp-14"
 
 # for cases that don't run "lunch linaro_db845c-userdebug"
@@ -25,21 +26,10 @@ if [ ! -d "${INSTALLER_DIR}/${FIRMWARE_DIR}/" ]; then
 fi
 
 pushd "${INSTALLER_DIR}/${FIRMWARE_DIR}" > /dev/null
-./flashall
+sudo "${QDL}" prog_firehose_ddr.elf rawprogram[012345].xml patch[012345].xml
 popd > /dev/null
 
-# Set HDMI monitor output
-echo "FLASH-ALL-AOSP: Set HDMI monitor output"
-fastboot oem select-display-panel foobar
-fastboot reboot bootloader
-
-# Slot _a is already marked as active by bootloader but just in case..
-echo "FLASH-ALL-AOSP: Mark _a slot as active"
-fastboot set_active a
-echo "FLASH-ALL-AOSP: Flash super/dynamic image"
-fastboot flash super "${ANDROID_PRODUCT_OUT}"/super.img
-echo "FLASH-ALL-AOSP: Flash userdata image"
-fastboot flash userdata "${ANDROID_PRODUCT_OUT}"/userdata.img
-echo "FLASH-ALL-AOSP: Flash boot image"
-fastboot flash boot "${ANDROID_PRODUCT_OUT}"/boot.img
-fastboot reboot
+echo
+echo
+echo "RECOVERY: Please boot the db845c into fastboot mode, and use the flash-all-aosp.sh script!"
+echo
