c0bf7a4b865efd32ff885a0ca2392fa73b8100a8
Amit Pundir <amit.pundir@linaro.org>
2019-08-28 14:58:39 +0530

    db845c: flash-all-aosp.sh: Add option to generate and flash boot.img
    Add option to generate and flash boot.img image, by passing kernel
    image as an argument to the script.
    
    Change-Id: Ib9f842e5d54e391e9aaaba3e6dfa01226d4ed9f4
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>

--- installer/db845c/flash-all-aosp.sh
+++ installer/db845c/flash-all-aosp.sh
@@ -11,7 +11,7 @@ if [ -z "${ANDROID_BUILD_TOP}" ]; then
 fi
 
 if [ ! -d "${ANDROID_PRODUCT_OUT}" ]; then
-    echo "error in locating out directory, check if it exist"
+    echo "FLASH-ALL-AOSP: error in locating out directory, check if it exist"
     exit
 fi
 
@@ -20,7 +20,7 @@ echo "android out dir:${ANDROID_PRODUCT_OUT}"
 # TODO: Pull one-time recovery/qdl path out of standard install
 # Flash bootloader firmware files
 if [ ! -d "${INSTALLER_DIR}/${FIRMWARE_DIR}/" ]; then
-    echo "No firmware directory? Make sure binaries have been provided"
+    echo "FLASH-ALL-AOSP: No firmware directory? Make sure binaries have been provided"
     exit
 fi
 
@@ -40,3 +40,15 @@ echo "FLASH-ALL-AOSP: Flash super/dynamic image"
 fastboot flash super "${ANDROID_PRODUCT_OUT}"/super.img
 echo "FLASH-ALL-AOSP: Flash userdata image"
 fastboot flash userdata "${ANDROID_PRODUCT_OUT}"/userdata.img
+
+if [ "$1" != "" ]; then
+    ANDROID_BUILD_TOP=${INSTALLER_DIR}/../../../../../
+    ANDROID_OUT_HOST_BIN="${ANDROID_BUILD_TOP}/out/host/linux-x86/bin"
+
+    echo "FLASH-ALL-AOSP: Building boot image with user provided ${1} kernel image"
+    ${ANDROID_OUT_HOST_BIN}/mkbootimg --kernel ${1} --ramdisk ${ANDROID_PRODUCT_OUT}/ramdisk.img --base 0x80000000 --pagesize 2048 --cmdline "firmware_class.path=/vendor/firmware/ androidboot.hardware=db845c init=/init androidboot.boot_devices=soc/1d84000.ufshc printk.devkmsg=on buildvariant=userdebug" --output ${ANDROID_PRODUCT_OUT}/boot.img
+
+    echo "FLASH-ALL-AOSP: Flash boot image"
+    fastboot flash boot ${ANDROID_PRODUCT_OUT}/boot.img
+    fastboot reboot
+fi
