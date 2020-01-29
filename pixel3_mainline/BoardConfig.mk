e6d9dfdcd2cb440d558e381742b12928f60a0234
John Stultz <john.stultz@linaro.org>
2020-01-29 00:23:28 +0000

    pixel3_maineline: Add changes to get pixel3_mainline building a boot.img (v2)
    This adds some changes to get the pixel3_mainline target
    building a boot.img. It requires that a pixel3_mainline dir be
    present in the dragonboard-kernel dir, and it should contain
    a Image.gz-dtb file and any kernel modules to be included.
    
    If you do not want to use kernel modules, build with
    PIXEL3_USES_GKI=false.
    
    This is take number two, as the first pass was causing build
    breakage. In this version, I check to make sure we have a
    kernel image before we try to build
    
    Change-Id: I5810ee98ce313521bbe56831cb298b55674530fc
    Signed-off-by: John Stultz <john.stultz@linaro.org>

--- pixel3_mainline/BoardConfig.mk
+++ pixel3_mainline/BoardConfig.mk
@@ -4,6 +4,18 @@ include device/linaro/dragonboard/BoardConfigCommon.mk
 TARGET_BOOTLOADER_BOARD_NAME := pixel3_mainline
 TARGET_BOARD_PLATFORM := pixel3_mainline
 
+ifeq ($(PIXEL3_KERNEL_FOUND), true)
+    # Bootloader/Kernel Configuration
+    TARGET_NO_KERNEL         := false
+    BOARD_KERNEL_BAS         := 0x00000000
+    BOARD_KERNEL_PAGESIZE    := 4096
+    BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
+    BOARD_RAMDISK_OFFSET     := 0x02000000
+    BOARD_KERNEL_CMDLINE     := firmware_class.path=/vendor/firmware/ androidboot.hardware=pixel3_mainline efi=noruntime
+    BOARD_KERNEL_CMDLINE     += init=/init androidboot.boot_devices=soc/1d84000.ufshc printk.devkmsg=on
+    BOARD_KERNEL_CMDLINE     += androidboot.super_partition=system_b #system_b is super partition
+endif
+
 # Image Configuration
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x04000000
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 10737418240
