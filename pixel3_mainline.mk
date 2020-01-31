471d30e6330f88e0b3a8cd32fd84f88dc00b8980
Todd Kjos <tkjos@google.com>
2020-01-31 04:07:37 +0000

    Revert "pixel3_maineline: Add changes to get pixel3_mainline building a boot.img"
    This reverts commit 85f6a8aecd3ba7609d28ace79f474a411092ebdd.
    
    Reason for revert: Caused build-break for aosp-master (pixel3_mainline-userdebug build)
    
    Change-Id: I653961de83baa953cfdde32e42d5706869764d9a

--- pixel3_mainline.mk
+++ pixel3_mainline.mk
@@ -1,5 +1,3 @@
-PIXEL3_KERNEL_DIR := device/linaro/dragonboard-kernel/pixel3_mainline/
-
 # Inherit the full_base and device configurations
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, device/linaro/dragonboard/pixel3_mainline/device.mk)
@@ -10,17 +8,3 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 PRODUCT_NAME := pixel3_mainline
 PRODUCT_DEVICE := pixel3_mainline
 PRODUCT_BRAND := Android
-
-ifndef PIXEL3_USES_GKI
-PIXEL3_USES_GKI := true
-endif
-
-ifeq ($(PIXEL3_USES_GKI), true)
-PIXEL3_MODS := $(wildcard $(PIXEL3_KERNEL_DIR)/*.ko)
-ifneq ($(PIXEL3_MODS),)
-  BOARD_VENDOR_KERNEL_MODULES += $(PIXEL3_MODS)
-
-  P3_ONLY_VENDOR := %/msm.ko
-  BOARD_VENDOR_RAMDISK_KERNEL_MODULES := $(filter-out $(P3_ONLY_VENDOR),$(PIXEL3_MODS))
-endif
-endif
