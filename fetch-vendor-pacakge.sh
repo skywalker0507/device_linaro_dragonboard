97b2d04315bf29efd935761c8fc97cb1af8fa01b
John Stultz <john.stultz@linaro.org>
2022-01-26 23:52:55 +0000

    dragonboard: Add helper script to simplify fetching the correct vendor-package
    Provide a script to help fetch, check and extract the vendor-package
    
    Signed-off-by: John Stultz <john.stultz@linaro.org>
    Change-Id: I928891d23841c103d31e7302d2bf667df6f24e66

---
+++ fetch-vendor-pacakge.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# fetch, check & extract the current vendor package
+set -e
+
+if [ -z "${ANDROID_BUILD_TOP}" ]; then
+    echo "Error: ANDROID_BUILD_TOP not defined, please run lunch"
+    exit
+fi
+
+. "${ANDROID_BUILD_TOP}/device/linaro/dragonboard/vendor-package-ver.sh"
+
+PKG_FILE=extract-linaro_devices-${EXPECTED_LINARO_VENDOR_VERSION}
+
+pushd ${ANDROID_BUILD_TOP}
+curl -L ${VND_PKG_URL} -o  ${PKG_FILE}.tgz
+
+# generate expected sha512sum, check & cleanup
+echo "${EXPECTED_LINARO_VENDOR_SHA}  ${PKG_FILE}.tgz" > ${PKG_FILE}.tgz.sha
+sha512sum -c ${PKG_FILE}.tgz.sha
+rm ${PKG_FILE}.tgz.sha
+
+tar -xf ${PKG_FILE}.tgz
+./${PKG_FILE}.sh
+popd
