skywalker0507/device_linaro_dragonboard

commit a0d7e989f15926e49e46b6c0595d5c753ff36c3e

Enable serial console on dragonboard devices.
A recent android-mainline kernel commit 01e249da0a52 ("ANDROID: GKI:
Enable CONFIG_NULL_TTY by default") enabled CONFIG_NULL_TTY in the
GKI, which caused the serial console to be disabled by default. This
is a reasonable default for production devices, but for development
boards it makes more sense to have it be enabled. Do so by passing
the appropriate console flags in the boot image.

Change-Id: I21e2a53a4f46a698060297dbff717a75816117df

db845c/BoardConfig.mk
@@ -34,6 +34,7 @@ BOARD_KERNEL_CMDLINE += init=/init androidboot.boot_devices=soc@0/1d84000.ufshc 
 BOARD_KERNEL_CMDLINE += deferred_probe_timeout=30
 BOARD_KERNEL_CMDLINE += pcie_pme=nomsi #For WiFi to work on rb5
 BOARD_KERNEL_CMDLINE += qcom_geni_serial.con_enabled=1
+BOARD_KERNEL_CMDLINE += console=ttyMSM0
 
 # Image Configuration
 BOARD_BOOTIMAGE_PARTITION_SIZE := 103079215104 #96M

rb5/BoardConfig.mk
@@ -30,6 +30,7 @@ BOARD_KERNEL_CMDLINE += init=/init androidboot.boot_devices=soc@0/1d84000.ufshc 
 BOARD_KERNEL_CMDLINE += pcie_pme=nomsi #For WiFi to work
 BOARD_KERNEL_CMDLINE += deferred_probe_timeout=30
 BOARD_KERNEL_CMDLINE += qcom_geni_serial.con_enabled=1
+BOARD_KERNEL_CMDLINE += console=ttyMSM0
 
 # Image Configuration
 BOARD_BOOTIMAGE_PARTITION_SIZE := 103079215104 #96M

sm8450/BoardConfig.mk
@@ -44,6 +44,7 @@ BOARD_MKBOOTIMG_ARGS := --header_version $(BOARD_BOOT_HEADER_VERSION)
 BOARD_KERNEL_CMDLINE := earlycon firmware_class.path=/vendor/firmware/ androidboot.hardware=sm8450
 BOARD_KERNEL_CMDLINE += init=/init androidboot.boot_devices=soc@0/1d84000.ufshc printk.devkmsg=on
 BOARD_KERNEL_CMDLINE += allow_mismatched_32bit_el0
+BOARD_KERNEL_CMDLINE += console=ttyMSM0
 
 # Image Configuration
 BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864 #64M

commit c2840b75d1c87ec39ef15b86cf9e92e41c992da1

db845c: Set baud rate for serial console
The default baud rate on kernel releases prior to v5.10
is 9600, which breaks serial console for db845c and AOSP
takes a loooooong time to boot.

Upstream resets default baud rate to 115200 in commit
2ec812a04ff4 ("tty: serial: qcom_geni_serial: 115.2 is a
better console default than 9600").

Fixes: a0d7e989f159 ("Enable serial console on dragonboard devices.")
Signed-off-by: Amit Pundir <amit.pundir@linaro.org>
Change-Id: I7f1f758a5a6ad2645f7b628e0ab2eb6effe0f7ed

db845c/BoardConfig.mk
@@ -34,7 +34,7 @@ BOARD_KERNEL_CMDLINE += init=/init printk.devkmsg=on
 BOARD_KERNEL_CMDLINE += deferred_probe_timeout=30
 BOARD_KERNEL_CMDLINE += pcie_pme=nomsi #For WiFi to work on rb5
 BOARD_KERNEL_CMDLINE += qcom_geni_serial.con_enabled=1
-BOARD_KERNEL_CMDLINE += console=ttyMSM0
+BOARD_KERNEL_CMDLINE += console=ttyMSM0,115200n8
 
 # Image Configuration
 BOARD_BOOTIMAGE_PARTITION_SIZE := 103079215104 #96M