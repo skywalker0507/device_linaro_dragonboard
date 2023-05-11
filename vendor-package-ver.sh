c7d89af231309ad3af42900592010810f5922ddf
Amit Pundir <amit.pundir@linaro.org>
2023-05-11 15:52:43 +0530

    dragonboards: Update vendor-package to 20230510 release
    Update to the 20230510 release, which update mesa prebuilt
    binaries to mesa-23.0.2 release tag.
    
    Note: These binaries are built with android13-gsi branch
          because the binaries built with AOSP (as of
          13-April-2023) ain't stable and are running into
          crashes like this one,
          https://gitlab.freedesktop.org/mesa/mesa/-/issues/8776,
          which can be reproduced by running "atest
          CtsUiRenderingTestCases" as well.
    
    vendor-package binary:
    https://releases.linaro.org/android/aosp-linaro-vendor-package/extract-linaro_devices-20230510.tgz
    
    vendor-package source:
    https://gitlab.com/LinaroLtd/linaro-aosp/linaro-vendor-package/-/commits/master
    
    Change-Id: I6b54cd04039b4cd05cd80e7da4dfa371b7154558
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>

--- vendor-package-ver.sh
+++ vendor-package-ver.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-export EXPECTED_LINARO_VENDOR_VERSION=20221126
+export EXPECTED_LINARO_VENDOR_VERSION=20230510
 #make sure to use sha512sum here
-export EXPECTED_LINARO_VENDOR_SHA=b87346f0612809458f556d0770f32a542ffd200418fe89bf1bf11b250f8c9197cd7c3624c87d141837c315a308d166129f711cf8c7b0a31eafcc1c2b87556199
-export VND_PKG_URL=https://releases.linaro.org/android/aosp-linaro-vendor-package/extract-linaro_devices-20221126.tgz
+export EXPECTED_LINARO_VENDOR_SHA=37aebd6fef4294226293ebe1945bd7a327b2653aaa8b294578d5a00141c1717f3aeb9b1b1832157d894193dd836e8c783b3fe0b4674aa9bd69fee3555d4ec0c3
+export VND_PKG_URL=https://releases.linaro.org/android/aosp-linaro-vendor-package/extract-linaro_devices-20230510.tgz
 
 if [ "$1" = "url" ]; then
  echo $VND_PKG_URL
