b2534ba9661d343fbf1e43e93e4e3d4667137a2d
Amit Pundir <amit.pundir@linaro.org>
2023-09-12 19:16:36 +0530

    linaro_swr: A generic build target using software rendering
    linaro_swr is an attempt to prepare a generic arm64 build
    target booting with software rendering support SwANGLE
    i.e. GLES implementation (ANGLE) on top of SwiftShader’s
    vulkan (Pastel) implementation.
    
    Though it is developed and tested for Qcom target devices
    for now, but the plan is to make it generic enough to boot
    on any generic arm64 device.
    
    Change-Id: I135b4d2148330368ff298cb5d4bd290a67a94422
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>

--- build/tasks/kernel.mk
+++ build/tasks/kernel.mk
@@ -1,4 +1,4 @@
-ifneq ($(filter db845c db845c_mini, $(TARGET_DEVICE)),)
+ifneq ($(filter db845c db845c_mini linaro_swr, $(TARGET_DEVICE)),)
 
 $(PRODUCT_OUT)/dtb.img: $(TARGET_KERNEL_DIR)/sdm845-db845c.dtb $(wildcard $(TARGET_KERNEL_DIR)/qrb5165-rb5.dtb)
 	cat $^ > $@
