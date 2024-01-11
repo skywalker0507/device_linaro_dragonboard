292fc0a6575a22d79ba75ef4bf79825dfe2a7a5d
Amit Pundir <amit.pundir@linaro.org>
2024-01-11 13:07:10 +0530

    db845c: Enable booting from mmc-sdcard
    This is an experimental build configuration and only used to
    boot AOSP on mmc sdcard on DB845c and RB5 in the LKFT lab.
    Regular users should not enable this build flag and should
    boot from UFS instead.
    
    Booting from external sdcards will help prevent the internal
    emmc/ufs wear off in the long run and extend the lab-life of
    most of our devboards.
    
    Set TARGET_SDCARD_BOOT=true at build time to build and boot
    AOSP from mmc sdcard. This device configuration need atleast
    8GB sdcard to boot from. As of today, 1st AOSP/main boot
    with Linux kernel v6.7 on 16GB Samsung EVO+ SDHCI sdcard
    takes just under 3mins to boot to UI and subsequent reboot
    to UI takes just over 1min.
    
    Using U-Boot to partition and flash mmc sdcard:
    To avoid flashing anything on internal UFS and boot solely
    from a sdcard, we are switching to chainloading U-Boot from
    ABL bootloader. For now we are using an upstream U-Boot fork
    for DB845c and RB5 and its currently under work-in-progress:
    https://source.devboardsforandroid.linaro.org/platform/external/u-boot/
    Since upstream U-Boot only support boot image header version
    2, so we are also using the same. It also helps us load and
    boot with boot.img from RAM using "fastboot boot boot.img"
    command in the lab. In the long run we plan to switch to
    AOSP/external/u-boot project to catch up with all the
    Android bootloader features. We are using U-Boot "gpt" and
    "fastboot" commands to create partition table on external
    sdcard and flash AOSP super and userdata images. Follow the
    U-Boot instructions here to prepare a sdcard to boot AOSP:
    https://devboardsforandroid.linaro.org/en/latest/devices/rb3.html#booting-aosp-from-mmc-sdcard
    
    Loading UFS driver from vendor_dlkm partition:
    The AOSP's first_stage_init mount take the first platform
    block device it finds and continues with it, irrespective of
    the androidboot.boot_devices bootarg. This is a blocker for
    us to boot from mmc sdcard because first_stage_init always
    tried to look for and mount UFS partitions. In order to
    avoid touching first_stage_init boot / mount flow in
    AOSP/system/core project, we will be loading UFS driver from
    vendor_dlkm partition so that UFS gets probed later in the
    boot cycle.
    
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>
    Change-Id: I20cd89df8b5f1fdb1bf712232284d6d754518d8f

--- db845c/BoardConfig.mk
+++ db845c/BoardConfig.mk
@@ -19,14 +19,26 @@ TARGET_BOARD_PLATFORM := db845c
 
 TARGET_NO_KERNEL := false
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
-ifeq ($(TARGET_USES_BOOT_HDR_V3), true)
- BOARD_BOOT_HEADER_VERSION := 3
- BOARD_KERNEL_CMDLINE += androidboot.hardware=db845c androidboot.boot_devices=soc@0/1d84000.ufshc
- BOARD_KERNEL_CMDLINE += androidboot.verifiedbootstate=orange
-else
- BOARD_BOOT_HEADER_VERSION := 4
- BOARD_BOOTCONFIG += androidboot.hardware=db845c androidboot.boot_devices=soc@0/1d84000.ufshc
- BOARD_BOOTCONFIG += androidboot.verifiedbootstate=orange
+ifeq ($(TARGET_SDCARD_BOOT), true)
+  BOARD_BOOT_HEADER_VERSION := 2
+  # Older kernel versions define mmc@ nodes as sdhci@ nodes in their device tree
+  ifneq ($(filter 5.4 5.10 5.15, $(TARGET_KERNEL_USE)),)
+    BOARD_KERNEL_CMDLINE += androidboot.boot_devices=soc@0/8804000.sdhci
+  else
+    BOARD_KERNEL_CMDLINE += androidboot.boot_devices=soc@0/8804000.mmc
+  endif
+  BOARD_KERNEL_CMDLINE += androidboot.hardware=db845c
+  BOARD_KERNEL_CMDLINE += androidboot.verifiedbootstate=orange
+else ifeq ($(TARGET_USES_BOOT_HDR_V3), true)
+    BOARD_BOOT_HEADER_VERSION := 3
+    BOARD_KERNEL_CMDLINE += androidboot.boot_devices=soc@0/1d84000.ufshc
+    BOARD_KERNEL_CMDLINE += androidboot.hardware=db845c
+    BOARD_KERNEL_CMDLINE += androidboot.verifiedbootstate=orange
+  else
+    BOARD_BOOT_HEADER_VERSION := 4
+    BOARD_BOOTCONFIG += androidboot.boot_devices=soc@0/1d84000.ufshc
+    BOARD_BOOTCONFIG += androidboot.hardware=db845c
+    BOARD_BOOTCONFIG += androidboot.verifiedbootstate=orange
 endif
 BOARD_MKBOOTIMG_ARGS := --header_version $(BOARD_BOOT_HEADER_VERSION)
 BOARD_KERNEL_BASE := 0x80000000
@@ -40,12 +52,18 @@ BOARD_KERNEL_CMDLINE += console=ttyMSM0,115200n8
 
 # Image Configuration
 BOARD_BOOTIMAGE_PARTITION_SIZE := 100663296 #96M
-BOARD_VENDOR_BOOTIMAGE_PARTITION_SIZE := 100663296 #96M
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 21474836480 #20G
 BOARD_FLASH_BLOCK_SIZE := 4096
-# Super partition
-BOARD_SUPER_PARTITION_SIZE := 12437225472
-BOARD_DB_DYNAMIC_PARTITIONS_SIZE := 12433031168 # Reserve 4M for DAP metadata
+ifeq ($(TARGET_SDCARD_BOOT), true)
+  BOARD_USERDATAIMAGE_PARTITION_SIZE := 2147483648 #2G
+  BOARD_SUPER_PARTITION_SIZE := 4294967296 #4G
+  BOARD_DB_DYNAMIC_PARTITIONS_SIZE := 4290772992 # Reserve 4M for DAP metadata
+  BOARD_SEPOLICY_DIRS += device/linaro/dragonboard/shared/utils/sdcard-boot/sepolicy/
+else
+  BOARD_VENDOR_BOOTIMAGE_PARTITION_SIZE := 100663296 #96M
+  BOARD_USERDATAIMAGE_PARTITION_SIZE := 21474836480 #20G
+  BOARD_SUPER_PARTITION_SIZE := 12437225472
+  BOARD_DB_DYNAMIC_PARTITIONS_SIZE := 12433031168 # Reserve 4M for DAP metadata
+endif
 BOARD_SUPER_PARTITION_METADATA_DEVICE := super
 BOARD_SUPER_IMAGE_IN_UPDATE_PACKAGE := true
 BOARD_DB_DYNAMIC_PARTITIONS_PARTITION_LIST := system vendor system_ext product
