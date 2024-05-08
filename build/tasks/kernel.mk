5b384af9533ad7052e46ed3242526fc1c5379d47
Amit Pundir <amit.pundir@linaro.org>
2024-05-08 13:24:04 +0530

    sm8x50: Add support for sm8450-{hdk,qrd} and sm8650-hdk
    Add support for SM8450-HDK, SM8450-QRD and SM8650-HDK
    Snapdragon 8 Gen devboards in sm8x50-userdebug build target
    
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>
    Change-Id: If224a42d9fe44d54bfe1dd9ab50ece083713f317

--- build/tasks/kernel.mk
+++ build/tasks/kernel.mk
@@ -2,24 +2,36 @@ ifneq ($(filter db845c db845c_mini linaro_swr sm8x50, $(TARGET_DEVICE)),)
 
 DTC := $(HOST_OUT)/bin/dtc
 FDTOVERLAY := $(HOST_OUT)/bin/fdtoverlay
+SM8450HDK_DTS_OVERLAY := device/linaro/dragonboard/sm8x50/dtb-overlays/sm8450-hdk.dts
+SM8450QRD_DTS_OVERLAY := device/linaro/dragonboard/sm8x50/dtb-overlays/sm8450-qrd.dts
 SM8550HDK_DTS_OVERLAY := device/linaro/dragonboard/sm8x50/dtb-overlays/sm8550-hdk.dts
 SM8550QRD_DTS_OVERLAY := device/linaro/dragonboard/sm8x50/dtb-overlays/sm8550-qrd.dts
+SM8650HDK_DTS_OVERLAY := device/linaro/dragonboard/sm8x50/dtb-overlays/sm8650-hdk.dts
 SM8650QRD_DTS_OVERLAY := device/linaro/dragonboard/sm8x50/dtb-overlays/sm8650-qrd.dts
 
 DB845C_DTB := $(TARGET_KERNEL_DIR)/sdm845-db845c.dtb
 RB5_DTB := $(wildcard $(TARGET_KERNEL_DIR)/qrb5165-rb5.dtb)
+SM8450HDK_DTB := $(wildcard $(TARGET_KERNEL_DIR)/sm8450-hdk.dtb)
+SM8450QRD_DTB := $(wildcard $(TARGET_KERNEL_DIR)/sm8450-qrd.dtb)
 SM8550HDK_DTB := $(wildcard $(TARGET_KERNEL_DIR)/sm8550-hdk.dtb)
 SM8550QRD_DTB := $(wildcard $(TARGET_KERNEL_DIR)/sm8550-qrd.dtb)
+SM8650HDK_DTB := $(wildcard $(TARGET_KERNEL_DIR)/sm8650-hdk.dtb)
 SM8650QRD_DTB := $(wildcard $(TARGET_KERNEL_DIR)/sm8650-qrd.dtb)
 
-$(PRODUCT_OUT)/dtb.img: $(DTC) $(FDTOVERLAY) $(SM8550HDK_DTB) $(SM8550QRD_DTB) $(SM8650QRD_DTB) $(RB5_DTB) $(DB845C_DTB)
+$(PRODUCT_OUT)/dtb.img: $(DTC) $(FDTOVERLAY) $(SM8450HDK_DTB) $(SM8450QRD_DTB) $(SM8550HDK_DTB) $(SM8550QRD_DTB) $(SM8650QRD_DTB) $(RB5_DTB) $(DB845C_DTB)
+	-$(DTC) -@ -I dts -O dtb -o $(PRODUCT_OUT)/sm8450-hdk.dtbo $(SM8450HDK_DTS_OVERLAY)
+	-$(FDTOVERLAY) -i $(SM8450HDK_DTB) -o $(PRODUCT_OUT)/sm8450-hdk.dtb $(PRODUCT_OUT)/sm8450-hdk.dtbo
+	-$(DTC) -@ -I dts -O dtb -o $(PRODUCT_OUT)/sm8450-qrd.dtbo $(SM8450QRD_DTS_OVERLAY)
+	-$(FDTOVERLAY) -i $(SM8450QRD_DTB) -o $(PRODUCT_OUT)/sm8450-qrd.dtb $(PRODUCT_OUT)/sm8450-qrd.dtbo
 	-$(DTC) -@ -I dts -O dtb -o $(PRODUCT_OUT)/sm8550-hdk.dtbo $(SM8550HDK_DTS_OVERLAY)
 	-$(FDTOVERLAY) -i $(SM8550HDK_DTB) -o $(PRODUCT_OUT)/sm8550-hdk.dtb $(PRODUCT_OUT)/sm8550-hdk.dtbo
 	-$(DTC) -@ -I dts -O dtb -o $(PRODUCT_OUT)/sm8550-qrd.dtbo $(SM8550QRD_DTS_OVERLAY)
 	-$(FDTOVERLAY) -i $(SM8550QRD_DTB) -o $(PRODUCT_OUT)/sm8550-qrd.dtb $(PRODUCT_OUT)/sm8550-qrd.dtbo
+	-$(DTC) -@ -I dts -O dtb -o $(PRODUCT_OUT)/sm8650-hdk.dtbo $(SM8650HDK_DTS_OVERLAY)
+	-$(FDTOVERLAY) -i $(SM8650HDK_DTB) -o $(PRODUCT_OUT)/sm8650-hdk.dtb $(PRODUCT_OUT)/sm8650-hdk.dtbo
 	-$(DTC) -@ -I dts -O dtb -o $(PRODUCT_OUT)/sm8650-qrd.dtbo $(SM8650QRD_DTS_OVERLAY)
 	-$(FDTOVERLAY) -i $(SM8650QRD_DTB) -o $(PRODUCT_OUT)/sm8650-qrd.dtb $(PRODUCT_OUT)/sm8650-qrd.dtbo
-	-cat $(DB845C_DTB) $(RB5_DTB) $(PRODUCT_OUT)/sm8550-hdk.dtb $(PRODUCT_OUT)/sm8550-qrd.dtb $(PRODUCT_OUT)/sm8650-qrd.dtb > $@
+	-cat $(DB845C_DTB) $(RB5_DTB) $(PRODUCT_OUT)/sm8450-hdk.dtb $(PRODUCT_OUT)/sm8450-qrd.dtb $(PRODUCT_OUT)/sm8550-hdk.dtb $(PRODUCT_OUT)/sm8550-qrd.dtb $(PRODUCT_OUT)/sm8650-hdk.dtb $(PRODUCT_OUT)/sm8650-qrd.dtb > $@
 	-rm $(PRODUCT_OUT)/sm8*dtb*
 
 droidcore: $(PRODUCT_OUT)/dtb.img
